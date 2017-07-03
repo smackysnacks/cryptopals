4ed319b6f5ef6e7d91829b5a7c73b33fe38f9d33
Mack Stump <mack.stump@gmail.com>
2017-07-03 16:00:20 -0400

    add solution to challenge7

---
+++ challenges/challenge7/src/main.rs
@@ -0,0 +1,17 @@
+extern crate base64;
+extern crate openssl;
+extern crate utils;
+
+use base64::decode;
+use openssl::symm::{decrypt, Cipher};
+
+fn main() {
+    let message = include_str!("../7.txt");
+    let message = message.replace("\n", "");
+    let message = decode(&message).unwrap();
+
+    let key = b"YELLOW SUBMARINE";
+    let cipher = Cipher::aes_128_ecb();
+    let result = decrypt(cipher, &key[..], None, &message);
+    println!("{}", String::from_utf8_lossy(&result.unwrap()));
+}
