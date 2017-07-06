5123a777c985935685dced695e0fac845707f353
Mack Stump <mack.stump@gmail.com>
2017-07-06 16:33:05 -0400

    add challenge10 solution

--- challenges/set2/challenge10/src/main.rs
+++ challenges/set2/challenge10/src/main.rs
@@ -1,3 +1,16 @@
+extern crate base64;
+extern crate utils;
+
+use base64::decode;
+
 fn main() {
-    println!("Hello, world!");
+    let message = include_str!("../10.txt");
+    let message = message.replace("\n", "");
+    let message = message.replace("\r", "");
+    let message = decode(&message).unwrap();
+    let key = b"YELLOW SUBMARINE";
+    let iv = [0; 16];
+
+    let decrypted = utils::aes::aes_128_cbc_decrypt(key, &message, &iv);
+    println!("{}", String::from_utf8_lossy(&decrypted));
 }
