e7d4a4a96ff26244d981055b4aef540fa45a9f19
Mack Stump <mack@mackstump.com>
2018-12-15 13:00:48 -0500

    upgrade to rust 2018 and change dir structure

--- lib/src/utils/counter.rs
+++ lib/src/utils/counter.rs
@@ -7,13 +7,15 @@ pub trait Counter {
     fn counts(&self) -> HashMap<&Self::Item, usize>;
 }
 
-impl<'a, T> Counter for &'a [T] where
-    T: Eq + Hash {
+impl<T> Counter for [T]
+where
+    T: Eq + Hash + Sized,
+{
     type Item = T;
 
     fn counts(&self) -> HashMap<&Self::Item, usize> {
         let mut counts = HashMap::new();
-        for k in *self {
+        for k in self {
             *counts.entry(k).or_insert(0) += 1;
         }
         counts
