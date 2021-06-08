82debc0d4c0e28d106bbdf4e50660f28d93a70db
Mack Stump <mack@mackstump.com>
2021-06-07 21:43:28 -0400

    solve challenge 11

--- crypto/src/utils/aes.rs
+++ crypto/src/utils/aes.rs
@@ -1,5 +1,7 @@
+use crate::utils::xor_repeating;
 use aes::Aes128;
 use block_modes::{block_padding::NoPadding, BlockMode, Ecb};
+use std::convert::TryInto;
 
 pub fn aes_128_ecb_encrypt(key: [u8; 16], data: &[u8]) -> Vec<u8> {
     let cipher = Ecb::<Aes128, NoPadding>::new_from_slices(&key, &[]).unwrap();
@@ -11,10 +13,22 @@ pub fn aes_128_ecb_decrypt(key: [u8; 16], data: &[u8]) -> Vec<u8> {
     cipher.decrypt_vec(data).unwrap()
 }
 
-pub fn aes_128_cbc_decrypt(key: [u8; 16], data: &[u8], iv: &[u8]) -> Vec<u8> {
+pub fn aes_128_cbc_encrypt(key: [u8; 16], data: &[u8], iv: [u8; 16]) -> Vec<u8> {
+    data.chunks(16)
+        .fold((Vec::with_capacity(data.len()), iv), |(mut v, c), chunk| {
+            let p: Vec<u8> = xor_repeating(chunk, &c).collect();
+            let encrypted_block = aes_128_ecb_encrypt(key, &p);
+            v.extend(encrypted_block.iter());
+
+            (v, encrypted_block.try_into().unwrap())
+        })
+        .0
+}
+
+pub fn aes_128_cbc_decrypt(key: [u8; 16], data: &[u8], iv: [u8; 16]) -> Vec<u8> {
     let mut dec_buf: Vec<u8> = Vec::with_capacity(data.len());
     let mut c = Vec::with_capacity(16);
-    c.extend(iv);
+    c.extend(iv.iter());
 
     let mut i = 0;
     while i * 16 < data.len() {
@@ -38,4 +52,22 @@ pub fn aes_128_cbc_decrypt(key: [u8; 16], data: &[u8], iv: &[u8]) -> Vec<u8> {
     }
 
     dec_buf
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::utils::{aes_128_cbc_decrypt, aes_128_cbc_encrypt};
+    use rand::random;
+
+    #[test]
+    fn test_aes_128_cbc_crypt() {
+        let key = random::<[u8; 16]>();
+        let iv = random::<[u8; 16]>();
+        let input: Vec<u8> = (0..256).map(|_| random::<u8>()).collect();
+
+        let encrypted = aes_128_cbc_encrypt(key, &input, iv);
+        let decrypted = aes_128_cbc_decrypt(key, &encrypted, iv);
+
+        assert_eq!(input, decrypted);
+    }
 }
--- src/challenge_sets/set2/challenge11.rs
+++ src/challenge_sets/set2/challenge11.rs
@@ -1,11 +1,43 @@
-use rand::Rng;
+use crypto::utils::{aes_128_cbc_encrypt, aes_128_ecb_encrypt, pkcs7_pad};
+use rand::random;
 
-fn random_key() -> [u8; 16] {
-    rand::thread_rng().gen()
+#[derive(Eq, PartialEq)]
+enum BlockMode {
+    Ecb,
+    Cbc,
+}
+
+fn random_data(min: u32, max: u32) -> Vec<u8> {
+    (min..=max).map(|_| random::<u8>()).collect()
+}
+
+fn encrypt(input: &[u8]) -> (Vec<u8>, BlockMode) {
+    let use_ecb = random::<bool>();
+    let key = random::<[u8; 16]>();
+    let prepend = random_data(5, 10);
+    let append = random_data(5, 10);
+
+    let mut data: Vec<u8> = prepend
+        .into_iter()
+        .chain(input.iter().cloned())
+        .chain(append.into_iter())
+        .collect();
+    pkcs7_pad(&mut data, 16);
+    match use_ecb {
+        true => (aes_128_ecb_encrypt(key, &data), BlockMode::Ecb),
+        false => (aes_128_cbc_encrypt(key, &data, random()), BlockMode::Cbc),
+    }
 }
 
 pub fn solve() -> bool {
-    let _key = random_key();
-
-    false
+    (0..50)
+        .map(|_| {
+            let (data, actual_mode) = encrypt(&[0; 64]);
+            let chunks: Vec<&[u8]> = data.chunks(16).skip(1).take(2).collect();
+            match chunks[0] == chunks[1] {
+                true => (BlockMode::Ecb, actual_mode),
+                false => (BlockMode::Cbc, actual_mode),
+            }
+        })
+        .all(|(expected, actual)| expected == actual)
 }
